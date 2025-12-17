72aad23e6bc9107f20c48b165bd24d31c91f5be4
shah-design-test <sharanr1@in.ibm.com>
2025-12-17 10:08:40 +0530

    Update prefect_job.py

--- prefect_job.py
+++ prefect_job.py
@@ -1,24 +1,26 @@
 from prefect import flow, task
+from prefect.logging import get_run_logger
 import time
 
-@task(retries=2, retry_delay_seconds=5)
+@task
 def step_one():
-    print("Step 1: Job started")
+    logger = get_run_logger()
+    logger.info("Step 1: Job started")
 
 @task
 def step_two():
-    print("Step 2: Processing...")
+    logger = get_run_logger()
+    logger.info("Step 2: Processing...")
     time.sleep(3)
 
 @task
 def step_three():
-    print("Step 3: Job completed")
+    logger = get_run_logger()
+    logger.info("Step 3: Job completed")
 
 @flow(name="sample-prefect-job")
 def my_job():
     step_one()
     step_two()
     step_three()
-
-if __name__ == "__main__":
-    my_job()
+my_job()
